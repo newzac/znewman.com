1d750dc2e921957b1833b24d482bf5c1a657768c
Zachary Newman <github@znewman.com>
2017-06-03 12:33:27 -0500

    make it executable

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-scp -rvc "ssh -o StrictHostKeyChecking=no"  ./css $ZUSER@$ZHOST:~$ZUSER/znewman.com/
-scp -rvc "ssh -o StrictHostKeyChecking=no"  ./img $ZUSER@$ZHOST:~$ZUSER/znewman.com/
-scp -rvc "ssh -o StrictHostKeyChecking=no"  ./js $ZUSER@$ZHOST:~$ZUSER/znewman.com/
-scp -rvc "ssh -o StrictHostKeyChecking=no"  ./less $ZUSER@$ZHOST:~$ZUSER/znewman.com/
-scp -rvc "ssh -o StrictHostKeyChecking=no"  ./vendor $ZUSER@$ZHOST:~$ZUSER/znewman.com/
-scp -vc "ssh -o StrictHostKeyChecking=no"  ./gulpfile.js $ZUSER@$ZHOST:~$ZUSER/znewman.com/
-scp -vc "ssh -o StrictHostKeyChecking=no"  ./index.html $ZUSER@$ZHOST:~$ZUSER/znewman.com/
-scp -vc "ssh -o StrictHostKeyChecking=no"  ./package.json $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rv  -o StrictHostKeyChecking=no ./css $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rv  -o StrictHostKeyChecking=no ./img $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rv  -o StrictHostKeyChecking=no ./js $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rv  -o StrictHostKeyChecking=no ./less $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rv  -o StrictHostKeyChecking=no ./vendor $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -v   -o StrictHostKeyChecking=no ./gulpfile.js $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -v   -o StrictHostKeyChecking=no ./index.html $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -v   -o StrictHostKeyChecking=no ./package.json $ZUSER@$ZHOST:~$ZUSER/znewman.com/
