27902a05c54bf2b77c3e7eb7f6b396c60d51de42
Zachary Newman <github@znewman.com>
2017-06-03 12:31:19 -0500

    fix deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-rsync -arvzc --delete-after -m  "ssh -o StrictHostKeyChecking=no"  ./. $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rvc "ssh -o StrictHostKeyChecking=no"  ./css $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rvc "ssh -o StrictHostKeyChecking=no"  ./img $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rvc "ssh -o StrictHostKeyChecking=no"  ./js $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rvc "ssh -o StrictHostKeyChecking=no"  ./less $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -rvc "ssh -o StrictHostKeyChecking=no"  ./vendor $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -vc "ssh -o StrictHostKeyChecking=no"  ./gulpfile.js $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -vc "ssh -o StrictHostKeyChecking=no"  ./index.html $ZUSER@$ZHOST:~$ZUSER/znewman.com/
+scp -vc "ssh -o StrictHostKeyChecking=no"  ./package.json $ZUSER@$ZHOST:~$ZUSER/znewman.com/
